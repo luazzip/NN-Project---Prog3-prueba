618a558b6740dca89a546766b389df1c8914f8c8
Luana Yolanda Meniz Cueva <luana.meniz@utec.edu.pe>
2026-07-08 15:18:11 -0500

    Add overloaded conv2d with strides and padding
    Added overloaded conv2d function with strides and padding parameters.

--- include/utec/nn/nn_ops.h
+++ include/utec/nn/nn_ops.h
@@ -3,6 +3,9 @@
 #include "../algebra/tensor_ops.h"
 
 namespace utec::tf {
+    using ops::Padding;
+    using ops::Strides;
+
     inline Tensor<float> matmul(const Tensor<float>& a,
                                 const Tensor<float>& b) {
         return ops::matmul(a, b);
@@ -11,5 +14,12 @@ namespace utec::tf {
     inline Tensor<float> conv2d(const Tensor<float>& input,
                                 const Tensor<float>& kernel) {
         return ops::conv2d(input, kernel);
+    }
+
+    inline Tensor<float> conv2d(const Tensor<float>& input,
+                                const Tensor<float>& kernel,
+                                Strides strides,
+                                Padding padding) {
+        return ops::conv2d(input, kernel, strides, padding);
     }
 }
